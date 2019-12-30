bd909fb4039f3f227178cf3540f1a64ff8e8c72d
sachinm78 <sachinm78@gmail.com>
2019-12-30 10:36:49 -0600

    set up initial file structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+/**
+ * Challenge: build the basic structure of our game
+ * 
+ * 1. <h1> title at the top
+ * 2. <textarea> for the box to type in 
+ *      (tip: React normalizes <textarea /> to be more like <input />, 
+ *      so it can be used as a self-closing element and uses the `value` property
+ *      to set its contents)
+ * 3. <h4> ti display the amount of time remaining
+ * 4. <button> to start the game
+ * 5. Another <h1> to display the word count
+ */
