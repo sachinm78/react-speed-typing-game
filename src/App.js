af948e215f03c18d33040ae2395e48a825cc5a1e
sachinm78 <sachinm78@gmail.com>
2019-12-30 10:53:40 -0600

    added useState hook and event handler to update state when textarea changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from "react"
+import React, {useState} from "react"
 import styles from "./styles.css"
 
 function App() {
+    const [text, setText] = useState("")
+    
+    function handleChange(e) {
+        const {value} = e.target
+        setText(value)
+    }
+    
     return (
         <div>
             <h1>How fast do you type?</h1>
-            <textarea />
-            <h4>Time reminaing: ???</h4>
+            <textarea
+                onChange={handleChange}
+                value={text}
+            />
+            <h4>Time remaining: ???</h4>
             <button>Start</button>
             <h1>Word count: ???</h1>
         </div>
