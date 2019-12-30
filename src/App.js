79374b9d409ba544b1e0995329f7bf536556e648
sachinm78 <sachinm78@gmail.com>
2019-12-30 14:36:31 -0600

    added custom hook and refactored App.js to use the hook; game is running smoothly!

--- src/App.js
+++ src/App.js
@@ -1,47 +1,17 @@
 import React, {useState, useEffect, useRef} from "react"
+import useWordGame from "./hooks/useWordGame"
 import styles from "./styles.css"
 
 function App() {
-    const STARTING_TIME = 15
-    
-    const [text, setText] = useState("")
-    const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
-    const [isTimeRunning, setIsTimeRunning] = useState(false)
-    const [wordCount, setWordCount] = useState(0)
-    const textBoxRef = useRef(null)
-    
-    function handleChange(e) {
-        const {value} = e.target
-        setText(value)
-    }
-    
-    function calculateWordCount(text) {
-        const wordsArr = text.trim().split(" ")
-        return wordsArr.filter(word => word !== "").length
-    }
-    
-    function startGame() {
-        setIsTimeRunning(true)
-        setTimeRemaining(STARTING_TIME)
-        setText("")
-        textBoxRef.current.disabled = false
-        textBoxRef.current.focus()
-    }
-    
-    function endGame() {
-        setIsTimeRunning(false)
-        setWordCount(calculateWordCount(text))
-    }
-    
-    useEffect(() => {
-        if(isTimeRunning && timeRemaining > 0) {
-            setTimeout(() => {
-                setTimeRemaining(time => time - 1)
-            }, 1000)
-        } else if(timeRemaining === 0) {
-            endGame()
-        }
-    }, [timeRemaining, isTimeRunning])
+    const {
+        textBoxRef, 
+        handleChange, 
+        text, 
+        isTimeRunning, 
+        timeRemaining, 
+        startGame, 
+        wordCount
+    } = useWordGame()
     
     return (
         <div>
