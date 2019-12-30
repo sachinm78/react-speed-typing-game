b6cdf3523c4486de48570763224e867266cd34b3
sachinm78 <sachinm78@gmail.com>
2019-12-30 12:54:11 -0600

    created startClock function that resets the board and timer when start button is clicked

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import styles from "./styles.css"
 
 function App() {
     const [text, setText] = useState("")
-    const [timeRemaining, setTimeRemaining] = useState(2)
+    const [timeRemaining, setTimeRemaining] = useState(5)
     const [isTimeRunning, setIsTimeRunning] = useState(false)
     const [wordCount, setWordCount] = useState(0)
     
@@ -15,6 +15,12 @@ function App() {
     function calculateWordCount(text) {
         const wordsArr = text.trim().split(" ")
         return wordsArr.filter(word => word !== "").length
+    }
+    
+    function startClock() {
+        setIsTimeRunning(true)
+        setTimeRemaining(5)
+        setText("")
     }
     
     useEffect(() => {
@@ -36,7 +42,7 @@ function App() {
                 value={text}
             />
             <h4>Time remaining: {timeRemaining}</h4>
-            <button onClick={() => setIsTimeRunning(true)}>Start</button>
+            <button onClick={startClock}>Start</button>
             <h1>Word count: {wordCount}</h1>
         </div>
     )
